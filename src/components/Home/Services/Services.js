1d979b22de63f483d681144417e5ec4d9897747d
musasazib <musasazib@gmail.com>
2021-10-29 16:58:00 +0600

    Added Services

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -5,7 +5,7 @@ import './Services.css';
 const Services = () => {
     const [services, setServices] = useState([]);
     useEffect(() => {
-        fetch('https://raw.githubusercontent.com/musasazib/fake-dada/main/src/components/FitLab/productFakedata.json')
+        fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
